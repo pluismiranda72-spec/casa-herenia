57ceca20d2c279f05b20a51d5af3187eb93f4c21
Pedro Miranda <pluismiranda72@gmail.com>
2026-03-16 16:15:25 +0100

    🚀 Deploy: Panel de administración en /es/admin/calendario

--- app/admin/block-dates/page.tsx
+++
@@ -1,7 +0,0 @@
-import BlockDatesAdminClient from "./BlockDatesAdminClient";
-import { getManualBlockBookings } from "@/app/actions/blockDatesAdmin";
-
-export default async function BlockDatesAdminPage() {
-  const initialBlocks = await getManualBlockBookings();
-  return <BlockDatesAdminClient initialBlocks={initialBlocks} />;
-}
